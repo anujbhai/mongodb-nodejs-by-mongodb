9020da942862dc0b24d9c8568158132d38ddff36
ANUJ UPADHYAY <anuj.upadhyay@rsystems.com>
2022-10-20 21:03:52 +0530

    mongo-and-node-02 - aggregation and data analysis.

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 async function main() {
   const uri = process.env.DB_URI;
@@ -8,14 +8,16 @@ async function main() {
   try {
     await client.connect();
     // await listDatabases(client);
-    
+
+    await printCheapestSuburbs(client, "Australia", "Sydney", 10);
+
     // await createListing(client, {
     //   name: "A Lovely Loft",
     //   summary: "A charming loft in Paris.",
     //   bedrooms:  1,
     //   bathrooms: 1
     // });
-    
+
     // await createMultipleListings(client, [
     //   {
     //     name: "Infinite Views",
@@ -41,7 +43,7 @@ async function main() {
     // ]);
 
     // await deleteListingByName(client, "Cosy Cottage");
-    await deleteListingScrapedBeforeDate(client, new Date("2019-02-15"));
+    // await deleteListingScrapedBeforeDate(client, new Date("2019-02-15"));
 
     // await updateAllForPropertyType(client);
 
@@ -59,7 +61,7 @@ async function main() {
     //   maximumNoOfResults: 5
     // });
     // await updateListingByName(client, "Infinite Views", {bedrooms: 6, beds: 8});
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   } finally {
     await client.close();
@@ -71,14 +73,14 @@ main().catch(console.error);
 // DELETE FUNCTIONS
 async function deleteListingScrapedBeforeDate(client, date) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").deleteMany(
-    {"last_scraped": {$lt: date}}
+    { "last_scraped": { $lt: date } }
   );
 
   console.log(`${result.deletedCount} document(s) was/were deleted`);
 }
 
 async function deleteListingByName(client, nameOfListing) {
-  const result = await client.db("sample_airbnb").collection("listingsAndReviews").deleteOne({name: nameOfListing});
+  const result = await client.db("sample_airbnb").collection("listingsAndReviews").deleteOne({ name: nameOfListing });
 
   console.log(`${result.deletedCount} document(s) was/were deleted`);
 }
@@ -86,8 +88,8 @@ async function deleteListingByName(client, nameOfListing) {
 // UPDATE FUNCTIONS
 async function updateAllForPropertyType(client) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateMany(
-    {property_type: {$exists: false}},
-    {$set: {property_type: "Unknown"}}
+    { property_type: { $exists: false } },
+    { $set: { property_type: "Unknown" } }
   );
 
   console.log(`${result.matchedCount} document(s) matched the query criteria`);
@@ -96,9 +98,9 @@ async function updateAllForPropertyType(client) {
 
 async function upsertListingByName(client, nameOfListing, updatedListing) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateOne(
-    {name: nameOfListing},
-    {$set: updatedListing},
-    {upsert: true}
+    { name: nameOfListing },
+    { $set: updatedListing },
+    { upsert: true }
   );
 
   console.log(`${result.matchedCount} document(s) matched the query criteria`);
@@ -112,8 +114,8 @@ async function upsertListingByName(client, nameOfListing, updatedListing) {
 
 async function updateListingByName(client, nameOfListing, updatedListing) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateOne(
-    {name: nameOfListing},
-    {$set: updatedListing}
+    { name: nameOfListing },
+    { $set: updatedListing }
   );
 
   console.log(`${result.matchedCount} document(s) matched the query criteria`);
@@ -129,10 +131,10 @@ async function findListingsWithMinCriteria(client, {
   const cursor = await client.db("sample_airbnb")
     .collection("listingsAndReviews")
     .find({
-      bedrooms: {$gte: minNoOfBedrooms},
-      bathrooms: {$gte: minNoOfBathrooms}
+      bedrooms: { $gte: minNoOfBedrooms },
+      bathrooms: { $gte: minNoOfBathrooms }
     })
-    .sort({last_review: -1})
+    .sort({ last_review: -1 })
     .limit(maximumNoOfResults);
 
   const result = await cursor.toArray();
@@ -181,6 +183,43 @@ async function createListing(client, newListing) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
 
   console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+// AGGREGATION PIPELINE
+async function printCheapestSuburbs(client, country, market, maxLimit) {
+  const pipeline = [
+    {
+      '$match': {
+        'bedrooms': 1,
+        'address.country': country,
+        'address.market': market,
+        'address.suburb': {
+          '$exists': 1,
+          '$ne': ''
+        },
+        'room_type': 'Entire home/apt'
+      }
+    }, {
+      '$group': {
+        '_id': '$address.suburb',
+        'averagePrice': {
+          '$avg': '$price'
+        }
+      }
+    }, {
+      '$sort': {
+        'averagePrice': 1
+      }
+    }, {
+      '$limit': maxLimit
+    }
+  ];
+
+  const aggcursor = client.db("sample_airbnb").collection("listingsAndReviews").aggregate(pipeline);
+
+  await aggcursor.forEach(item => {
+    console.log(`${item._id}: ${item.averagePrice}`);
+  });
 }
 
 // LISTING AVAILABLE DBs
